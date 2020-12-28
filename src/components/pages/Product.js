5a04368ae229ae69990f4d949b2fc78a45c18f91
worjs320 <worjs320@naver.com>
2020-12-28 20:22:56 +0900

    Product Page Design

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -1,93 +1,96 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import ListAltIcon from "@material-ui/icons/ListAlt";
+import * as React from "react";
+import { DataGrid } from "@material-ui/data-grid";
 import Footer from "../Footer";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 140 },
+  { field: "lastName", headerName: "Last name", width: 140 },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 80,
   },
-  margin: {
-    margin: 20,
-    height: "100vh",
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 200,
+    valueGetter: (params) =>
+      `${params.getValue("firstName") || ""} ${
+        params.getValue("lastName") || ""
+      }`,
   },
-});
-
-function createData(name, calories, fat, carbs, protein, test) {
-  return { name, calories, fat, carbs, protein, test };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("123", 356, 16.0, 49, 3.9, 123123),
 ];
 
-export default function DenseTable() {
-  const classes = useStyles();
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxieasdasdasdasdasda", firstName: "Harvey", age: 65 },
+  { id: 10, lastName: "Roxiee", firstName: "Harvey", age: 65 },
+  { id: 11, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 12, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 13, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
 
+const useStyles = makeStyles((theme) => ({
+  mainContent: {
+    minHeight: "100vh",
+    marginTop: "20px;",
+  },
+  titleContent: {
+    marginBottom: "30px;",
+  },
+}));
+
+export default function DataTable() {
+  const classes = useStyles();
   return (
     <>
-      <div className={classes.margin}>
-        <Typography variant="h5" component="h5" gutterBottom>
-          <ListAltIcon /> Product List
-        </Typography>
-        <TableContainer component={Paper}>
-          <Table
-            className={classes.table}
-            size="small"
-            aria-label="a dense table"
+      <Container className={classes.mainContent}>
+        <Container
+          maxWidth="sm"
+          component="main"
+          className={classes.titleContent}
+        >
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="textPrimary"
+            gutterBottom
           >
-            <TableHead>
-              <TableRow>
-                <TableCell>Dessert (100g serving)</TableCell>
-                <TableCell align="right">Calories</TableCell>
-                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                <TableCell align="right">test&nbsp;(g)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.name}>
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="right">{row.calories}</TableCell>
-                  <TableCell align="right">{row.fat}</TableCell>
-                  <TableCell align="right">{row.carbs}</TableCell>
-                  <TableCell align="right">{row.protein}</TableCell>
-                  <TableCell align="right">{row.test}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
+            Product
+          </Typography>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            component="p"
+          >
+            Product List
+          </Typography>
+        </Container>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={10}
+          autoHeight
+          checkboxSelection
+          showToolbar
+        />
+      </Container>
       <Footer />
     </>
   );
